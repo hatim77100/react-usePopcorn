6fc646346ea5aa7fec4fac2aaf1593d040e3a99f
hatim77100 <najoua-hatim@hotmail.fr>
2023-12-08 10:22:52 +0100

    :package: add StarRating & use propTypes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from './App';
+import "./index.css";
+import App from "./App";
 
 import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <App />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" defaultRating={3} />
   </React.StrictMode>
 );
